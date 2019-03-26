dad92d788e45d45e69fe4389d72f9995bf8baed8
sasi kiran malladi <sasikiran.malladi@users.noreply.github.com>
2019-03-26 18:16:19 -0400

    camunda bpm 1st commit

---
+++ camunda/camunda/camunda/src/main/java/com/bpm/camunda/CamundaWebApp.java
@@ -0,0 +1,19 @@
+package com.bpm.camunda;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableProcessApplication
+public class CamundaWebApp 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(CamundaWebApp.class, args);
+    }
+}
--- camunda/demo/src/main/java/com/example/demo/DemoApplication.java
+++ camunda/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
